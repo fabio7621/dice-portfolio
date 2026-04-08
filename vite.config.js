f350b6c5df366cc08cf1cb11c519c6971b67a59f
fabio7621 <d6200121a@gmail.com>
2026-04-08 16:56:36 +0800

    upadate

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/dice-portfolio/',
   plugins: [vue()],
   test: {
     environment: 'jsdom',
